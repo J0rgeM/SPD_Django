d7dc973ddaf7aa75085d286a51c2365ad3de4e10
J0rgeM <jorgemvsilva2002@gmail.com>
2023-05-20 01:11:15 +0100

    forum spd

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.views import generic
 from .models import Post
+from django.shortcuts import render, redirect
+from .forms import PostForm
+from django.contrib.auth.models import User
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
@@ -8,3 +11,15 @@ class PostList(generic.ListView):
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('home')  # Replace 'post_detail' with your desired post detail view name
+    else:
+        form = PostForm()
+    return render(request, 'create_post.html', {'form': form})
