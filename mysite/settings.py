035f6ec7891ed7d096b408f39b00fbe1c691fccc
J0rgeM <jorgemvsilva2002@gmail.com>
2023-05-20 02:05:18 +0100

    forum spdv9

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,8 +80,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3_old',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
