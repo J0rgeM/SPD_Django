af0dd8651b6c4f8b5d48037981dac5c268adf9e7
J0rgeM <jorgemvsilva2002@gmail.com>
2023-05-20 01:53:15 +0100

    forum spdv8

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from .models import Post
 from django.shortcuts import render, redirect
 from .forms import PostForm
 from django.contrib.auth.models import User
-from django.views.decorators.csrf import csrf_protect
+
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
@@ -12,13 +12,12 @@ class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
 
-@csrf_protect
+
 def create_post(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
-            post = form.save(commit=False)
-            post.save()
+            post = form.save()
             return redirect('home')  # Replace 'post_detail' with your desired post detail view name
     else:
         form = PostForm()
